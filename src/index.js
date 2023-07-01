c4ef50b48db9fc4b9208f20983098f5c7030daac
William Norboe <william.norboe@gmail.com>
2023-07-01 08:24:11 -0700

    displays weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import './style.css';
 
 let image = document.querySelector("#wimg");
 let searchBtn = document.querySelector("#search");
+let tempInfoDiv = document.querySelector(".tempInfo");
+let tempType = document.querySelector("#temperature");
 
-searchBtn.addEventListener("click", ()=>{loadImage()});
+searchBtn.addEventListener("click", ()=>{
+    loadImage();
+    loadWeather();
+});
+
 async function loadImage(){
     let search = document.querySelector('#locationInput').value;
-    if(search == ""){
+    if(search === ""){
         search = 'London';
     }
     let response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=rj5Y4vSo1SUlJFJwJ2fWse13jRvpyZfQ&s='+search, {mode: 'cors'});
@@ -15,4 +21,26 @@ async function loadImage(){
     image.src = imgData.data.images.original.url;
 }
 
+async function loadWeather(){
+    let search = document.querySelector('#locationInput').value;
+    if(search === ""){
+        search = 'London';
+    }
+    let response = await fetch('https://api.weatherapi.com/v1/current.json?key=7c5bf67a3b524d7084a183409233006&q='+search, {mode: 'cors'});
+    let weatherData = await response.json();
+    console.log(weatherData);
+    if(weatherData.current == undefined){
+        tempInfoDiv.textContent = "No matching location found.";
+        return;
+    }
+    console.log(tempType.value);
+    let currentTemp = weatherData.current.temp_f + "°F";
+    if(tempType.value === "celcius"){
+        currentTemp = weatherData.current.temp_c + "°C";
+    }
+    tempInfoDiv.textContent = currentTemp + " in " + weatherData.location.name + " Right Now";
+    // image.src = imgData.data.images.original.url;
+}
+
+loadWeather();
 loadImage();
